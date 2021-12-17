ca9255663e53907ac61232c4b1f95db5713e39b8
Tyler Fabian <tylerfabian@Tylers-MacBook-Pro.local>
2021-12-17 13:10:20 -0800

    Added timline and hidden funcitonality to the first button

--- source.js
+++ source.js
@@ -4,6 +4,12 @@ var modal = document.getElementById("text1");
 // Get the button that opens the modal
 var btn = document.getElementById("btn1");
 
+const btn14 = document.getElementById("14");
+const btn15 = document.getElementById("15");
+const btn16 = document.getElementById("16");
+const btn17 = document.getElementById("17");
+const btn18 = document.getElementById("18");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
@@ -22,4 +28,12 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+btn14.onclick = function () {
+    btn.hidden = false;
+}
+
+btn15.onclick = function () {
+    btn.hidden = true;
 }
