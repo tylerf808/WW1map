b533f882b7360a937aa3633c768bdd30cd8344af
Tyler Fabian <tylerfabian@Tylers-MacBook-Pro.local>
2021-12-17 19:16:02 -0800

    Added one btn for each year and proper btns show for each year

--- source.js
+++ source.js
@@ -2,8 +2,13 @@
 var modal = document.getElementById("text1");
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn1");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var btn5 = document.getElementById("btn5");
 
+// Get buttons for the year
 const btn14 = document.getElementById("14");
 const btn15 = document.getElementById("15");
 const btn16 = document.getElementById("16");
@@ -14,7 +19,7 @@ const btn18 = document.getElementById("18");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
+btn1.onclick = function () {
     modal.style.display = "block";
 }
 
@@ -31,9 +36,41 @@ window.onclick = function (event) {
 }
 
 btn14.onclick = function () {
-    btn.hidden = false;
+    btn1.hidden = false;
+    btn2.hidden = true;
+    btn3.hidden = true;
+    btn4.hidden = true;
+    btn5.hidden = true;
 }
 
 btn15.onclick = function () {
-    btn.hidden = true;
+    btn1.hidden = true;
+    btn2.hidden = false;
+    btn3.hidden = true;
+    btn4.hidden = true;
+    btn5.hidden = true;
+}
+
+btn16.onclick = function () {
+    btn1.hidden = true;
+    btn2.hidden = true;
+    btn3.hidden = false;
+    btn4.hidden = true;
+    btn5.hidden = true;
+}
+
+btn17.onclick = function () {
+    btn1.hidden = true;
+    btn2.hidden = true;
+    btn3.hidden = true;
+    btn4.hidden = false;
+    btn5.hidden = true;
+}
+
+btn18.onclick = function () {
+    btn1.hidden = true;
+    btn2.hidden = true;
+    btn3.hidden = true;
+    btn4.hidden = true;
+    btn5.hidden = false;
 }
